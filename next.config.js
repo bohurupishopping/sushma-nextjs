4df33438027399890c1d181d4f82484f560cbeff
Bohurupi Shopping <official.bohurupi@gmail.com>
2025-03-14 08:23:43 +0530

    e

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  // Add trailingSlash for better static export compatibility
-  trailingSlash: true,
-  // Remove deprecated appDir option as it's now the default in Next.js 13+
-  experimental: {
-    // The appDir option has been removed
-  }
 };
 
 module.exports = nextConfig;
